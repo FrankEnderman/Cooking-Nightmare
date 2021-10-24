39cb394358fdf1e28d2c63977184010952f1662c
FrankEnderman <90335574+FrankEnderman@users.noreply.github.com>
2021-10-24 08:47:53 +0530

    retooled it to calculate how much cookies you can make

--- Main.java
+++ Main.java
@@ -5,19 +5,18 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("starting cooking nightmare 1.2 Alpha");
-		int EggAmount;
-		int EggMin = 1;
+		int ChocoAmount;
+		int ChocoMin = 3;
 		int flourAmount;
-		int flourMin = 100;
-		int MilkMin = 100;
+		int flourMin = 30;
+		int MilkMin = 10;
 		int MilkAmount;
-		int RiceAmount;
-		int RiceMin = 3;
-		System.out.println("how many eggs do you have?");
+		
+		System.out.println("how many grams of chocolate do you have?");
 		Scanner userInput;
 		userInput = new Scanner(System.in);
-		EggAmount = userInput.nextInt();
-		System.out.println("you have " + EggAmount + " eggs in your stash");
+		ChocoAmount = userInput.nextInt();
+		System.out.println("you have " + ChocoAmount + " grams of chocolate in your stash");
 		System.out.println("how much milk do you have?");
 		Scanner userInput1;
 		userInput1 = new Scanner(System.in);
@@ -28,16 +27,11 @@ public class Main {
 		userInput11 = new Scanner(System.in);
 		flourAmount = userInput11.nextInt();
 		System.out.println("you have " + flourAmount + " grams of flour in your stash");
-		System.out.println("how many KGs of rice do you have?");
-		Scanner userInput111;
-		userInput111 = new Scanner(System.in);
-		RiceAmount = userInput111.nextInt();
 		
-		System.out.println("you have " + RiceAmount + " kilos of rice in your stash");
-		if (EggAmount < EggMin || EggAmount < EggMin || flourAmount < flourMin || MilkAmount < MilkMin || RiceAmount < RiceMin) {
-			System.out.println("no food today :(");
+		if (ChocoAmount < ChocoMin || flourAmount < flourMin || MilkAmount < MilkMin) {
+			System.out.println("no cookies today :(");
 		} else {
-			System.out.println("food!!! :D");
+			System.out.println("Cookies!!! :D");
 		}
 	
 		}
